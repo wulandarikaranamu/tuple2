0e7bc61bfe771811a3bfa601e7a59e59d803280f
wulandarikaranamu <wulandarikaranamu02@gmail.com>
2022-02-26 22:02:21 +0700

    komentar

---
+++ tuple2.py
@@ -0,0 +1,23 @@
+# membuat tuple
+nama = ('merry', 'wulan', 'dari')
+
+# mengakses nilai tuple
+print(nama[1])
+
+
+# Mengambil panjang tuple
+print("panjang nama: %d" % len(nama))
+
+#cara cek type data
+data = ('a','b','c','d',1,2,3,4)
+print (data)
+print (type(data))
+
+# cek apakah c termasuk ke dalam nilai tuple 
+print('c' in data) 
+ 
+#cek apakah c tidak termasuk kedalam nilai tuple
+print('c' not in data)
+ 
+# cek apakah z termasuk kedalam nilai tuple
+print ('z' in data)
